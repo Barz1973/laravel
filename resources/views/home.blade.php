11bf1415fc5e327bb674075bd830a0edd6f3bd87
Users <gvirnie@gmail.com>
2022-09-17 22:05:12 +0800

    initial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Virnie Prelim Project</title>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+<body>
+    <div class="text-center">
+        <nav class="nav justify-content-center">
+            <a href="{{url('/')}}" class="nav-link">Home</a>
+            <a href="{{url('/about')}}" class="nav-link">About Us</a>
+            <a href="{{url('/profile')}}" class="nav-link">Company Profile</a>
+        </nav>
+
+        <h1>Virnie Prelim Project Page</h1>
+
+        <div class="card shadow-sm mx-auto" style="width: 500px">
+            <div class="card-body">
+                <div class="card-title">Welcome Message</div>
+                <p>
+                    Hi Dear
+                </p>
+                <p>
+                Thak You for Visiting and Joining Our Team Hope That You Can Enjoy For Joining Our Team Don't Worry We Are All Here To Support and Help You 
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Virnie Prelim Project</title>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+<body>
+    <div class="text-center">
+        <nav class="nav justify-content-center">
+            <a href="{{url('/')}}" class="nav-link">Home</a>
+            <a href="{{url('/about')}}" class="nav-link">About Us</a>
+            <a href="{{url('/profile')}}" class="nav-link">Company Profile</a>
+        </nav>
+        <h1>Company Profile</h1>
+
+        <div class="card shadow-sm mx-auto" style="width: 500px">
+            <div class="card-body">
+                <div class="card-title">Company Profile</div>
+                <p>
+                    Hello Dear
+                </p>
+                <p>
+                    Our Company Are implementing good and high quality standard for our product to make it unique and nice to our buyer or customer.
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
